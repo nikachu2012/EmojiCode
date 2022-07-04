8414a61c0ee67f65c3d2745484e815c2aa5014e9
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-07-05 00:05:43 +0900

    [update] 数学タブの完成

--- 3-math/math_def.js
+++ 3-math/math_def.js
@@ -55,6 +55,10 @@
             "alt": "^"
           },
           "super"
+        ],
+        [
+          "あまり",
+          "remainder"
         ]
       ]
     },
@@ -68,8 +72,314 @@
     }
   ],
   "inputsInline": true,
-  "output": null,
-  "colour": 65,
+  "output": "Number",
+  "colour": 230,
   "tooltip": "計算処理をします。四則演算及び乗法に対応しています。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "string",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "string",
+      "text": "123"
+    }
+  ],
+  "output": "Number",
+  "colour": 230,
+  "tooltip": "数字入力です。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "math_etc1",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "select",
+      "options": [
+        [
+          {
+            "src": "https://img.icons8.com/color/48/000000/square-root.png",
+            "width": 16,
+            "height": 16,
+            "alt": "ルート"
+          },
+          "root"
+        ],
+        [
+          "絶対値",
+          "absolute"
+        ],
+        [
+          {
+            "src": "https://img.icons8.com/color/48/000000/minus-math.png",
+            "width": 16,
+            "height": 16,
+            "alt": "*"
+          },
+          "minus"
+        ],
+        [
+          "ln",
+          "log"
+        ],
+        [
+          "log10",
+          "log10"
+        ],
+        [
+          "e^",
+          "exp"
+        ],
+        [
+          "10^",
+          "OPTIONNAME"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "input",
+      "check": "Number"
+    }
+  ],
+  "output": "Number",
+  "colour": 230,
+  "tooltip": "数学のいろいろな計算が簡単にできます。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "math_etc2",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "select",
+      "options": [
+        [
+          "sin",
+          "sin"
+        ],
+        [
+          "cos",
+          "cos"
+        ],
+        [
+          "tan",
+          "tan"
+        ],
+        [
+          "asin",
+          "asin"
+        ],
+        [
+          "acos",
+          "acos"
+        ],
+        [
+          "atan",
+          "atan"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "input",
+      "check": "String"
+    }
+  ],
+  "output": "Number",
+  "colour": 230,
+  "tooltip": "数学のいろいろな計算が簡単にできます。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "const_return",
+  "message0": "%1",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "NAME",
+      "options": [
+        [
+          "π",
+          "pi"
+        ],
+        [
+          "e",
+          "e"
+        ],
+        [
+          "φ",
+          "phi"
+        ],
+        [
+          "∞",
+          "infinity"
+        ]
+      ]
+    }
+  ],
+  "output": "String",
+  "colour": 230,
+  "tooltip": "いろいろな定数を返します",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "hantei_boolean",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "NAME",
+      "check": "Number"
+    },
+    {
+      "type": "field_dropdown",
+      "name": "select",
+      "options": [
+        [
+          "は偶数",
+          "even"
+        ],
+        [
+          "は奇数",
+          "odd"
+        ],
+        [
+          "は素数",
+          "prime"
+        ],
+        [
+          "は整数",
+          "integer"
+        ],
+        [
+          "は正",
+          "true"
+        ],
+        [
+          "は負",
+          "false"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": true,
+  "output": "Boolean",
+  "colour": 230,
+  "tooltip": "入力に対する判定を変える",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "rewrite_input",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "select",
+      "options": [
+        [
+          "四捨五入",
+          "round"
+        ],
+        [
+          "切り上げ",
+          "roundup"
+        ],
+        [
+          "切り下げ",
+          "rounddown"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "input",
+      "check": "Number"
+    }
+  ],
+  "output": "Boolean",
+  "colour": 230,
+  "tooltip": "入力を変換します。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "list_search",
+  "message0": "リストの %1 %2",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "select",
+      "options": [
+        [
+          "合計",
+          "sum"
+        ],
+        [
+          "最小値",
+          "min"
+        ],
+        [
+          "最大値",
+          "max"
+        ],
+        [
+          "平均",
+          "avg"
+        ],
+        [
+          "中央値",
+          "median"
+        ],
+        [
+          "最頻値",
+          "mode"
+        ],
+        [
+          "標準偏差",
+          "deviasion"
+        ],
+        [
+          "ランダムの項目",
+          "random"
+        ]
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "NAME",
+      "check": "Array"
+    }
+  ],
+  "output": "Number",
+  "colour": 230,
+  "tooltip": "リストの中で指定された条件を返します。",
+  "helpUrl": "https://example.com"
+},
+{
+  "type": "random",
+  "message0": "%1 から %2 %3 までの乱数",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "start",
+      "check": "Number"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "end",
+      "check": "Number"
+    }
+  ],
+  "output": "Number",
+  "colour": 230,
+  "tooltip": "指定範囲の乱数",
   "helpUrl": "https://example.com"
 }]
--- 3-math/math_stub.js
+++ 3-math/math_stub.js
@@ -7,3 +7,73 @@ Blockly.JavaScript['formula'] = function(block) {
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['string'] = function(block) {
+  var text_string = block.getFieldValue('string');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['math_etc1'] = function(block) {
+  var dropdown_select = block.getFieldValue('select');
+  var value_input = Blockly.JavaScript.valueToCode(block, 'input', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['math_etc2'] = function(block) {
+  var dropdown_select = block.getFieldValue('select');
+  var value_input = Blockly.JavaScript.valueToCode(block, 'input', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['const_return'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['hantei_boolean'] = function(block) {
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_select = block.getFieldValue('select');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['rewrite_input'] = function(block) {
+  var dropdown_select = block.getFieldValue('select');
+  var value_input = Blockly.JavaScript.valueToCode(block, 'input', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['list_search'] = function(block) {
+  var dropdown_select = block.getFieldValue('select');
+  var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['random'] = function(block) {
+  var value_start = Blockly.JavaScript.valueToCode(block, 'start', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_end = Blockly.JavaScript.valueToCode(block, 'end', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
