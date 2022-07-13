168b7293400de5ba550c413973023568c532568e
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-07-13 23:28:34 +0900

    [add] notの追加

--- 1-logic/logic_stub.js
+++ 1-logic/logic_stub.js
@@ -44,8 +44,8 @@ Blockly.JavaScript[`equals_return_boolean`] = function(block) {
 
 Blockly.JavaScript[`not`] = function(block) {
   var value_name = Blockly.JavaScript.valueToCode(block, `NAME`, Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = `...`;
+
+  var code = `!(${value_name})`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
