4b373e5398458c2464732ab5499f3a254584e07f
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-07-20 19:14:53 +0900

    [add] list_createの追加

--- 5-list/list_stub.js
+++ 5-list/list_stub.js
@@ -1,6 +1,5 @@
-Blockly.JavaScript['list_create'] = function(block) {
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+Blockly.JavaScript[`list_create`] = function(block) {
+  var code = `[]`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
