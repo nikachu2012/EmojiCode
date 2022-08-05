07ea335031d8b89de5dbc0905c5614dd87412ee5
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-05 21:05:57 +0900

    [add] stubの生成を行うように変更

--- builder/watch.js
+++ builder/watch.js
@@ -54,12 +54,41 @@ import Blockly from 'Blockly'\n`;
         });
 
 
-        fs.writeFile('../emojisprite.js', alldata, function (err, result) {
+        fs.writeFile('../emojicode_def.js', alldata, function (err, result) {
             if (err) console.log('error', err);
         });
     });
 
+    glob('../**/*', (err, files) => {
 
+        let fileList = [];
+        files.forEach(file => {
+            if (!/^..\/(?:build|builder|defaultFunctions|test|\..*)/.test(file) && /^..\/.*\/.*_stub\..*/.test(file)) {
+                fileList.push(file)
+            }
+        });
+
+        let alldata =
+            `/* 
+    EmojiCode Block Stub
+    Created by nikachu2012(https://github.com/nikachu2012)
+    Create time: ${new Date()}
+*/\n
+
+import Blockly from 'Blockly'\n`;
+        fileList.forEach(element => {
+            try {
+                alldata += `${fs.readFileSync(element)} \n`;
+            } catch (err) {
+                console.log(err)
+            }
+        });
+
+
+        fs.writeFile('../emojicode_stub.js', alldata, function (err, result) {
+            if (err) console.log('error', err);
+        });
+    });
 
 }
 
