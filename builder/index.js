5f50e26e7ef7a4710e9013f2326cce56b2259b7d
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-15 10:58:05 +0900

    [update] ビルダーの更新

--- builder/index.js
+++ builder/index.js
@@ -3,28 +3,62 @@ const fs = require('fs');
 
 glob('../**/*', (err, files) => {
 
-    let fileList = [];
-    files.forEach(file => {
-        if (!/^..\/build/.test(file) && !/^..\/debug/.test(file) && /^..\/.*\/.*\..*/.test(file)) {
-            fileList.push(file)
-        }
-    });
+        let fileList = [];
+        files.forEach(file => {
+            if (!/^..\/(?:build|builder|defaultFunctions|test|\..*)/.test(file) && /^..\/.*\/.*_def\..*/.test(file)) {
+                fileList.push(file)
+            }
+        });
 
-    let alldata = 
-    `/* 
-    EmojiCode Sprite Controller
+        let alldata =
+            `/* 
+    EmojiCode Block Define
     Created by nikachu2012(https://github.com/nikachu2012)
     Create time: ${new Date()}
-*/\n`;
-    fileList.forEach(element => {
-        try {
-            alldata += fs.readFileSync(element);
-        } catch (err) {
-            console.log(err)
-        }
+*/\n
+
+import Blockly from 'blockly'\n`;
+        fileList.forEach(element => {
+            try {
+                alldata += `${fs.readFileSync(element)} \n`;
+            } catch (err) {
+                console.log(err)
+            }
+        });
+
+
+        fs.writeFile('../emojicode_def.js', alldata, function (err, result) {
+            if (err) console.log('error', err);
+        });
     });
 
-    fs.writeFile('../emojisprite.js', alldata, function (err, result) {
-        if (err) console.log('error', err);
+    glob('../**/*', (err, files) => {
+
+        let fileList = [];
+        files.forEach(file => {
+            if (!/^..\/(?:build|builder|defaultFunctions|test|\..*)/.test(file) && /^..\/.*\/.*_stub\..*/.test(file)) {
+                fileList.push(file)
+            }
+        });
+
+        let alldata =
+            `/* 
+    EmojiCode Block Stub
+    Created by nikachu2012(https://github.com/nikachu2012)
+    Create time: ${new Date()}
+*/\n
+
+import Blockly from 'blockly'\n`;
+        fileList.forEach(element => {
+            try {
+                alldata += `${fs.readFileSync(element)} \n`;
+            } catch (err) {
+                console.log(err)
+            }
+        });
+
+
+        fs.writeFile('../emojicode_stub.js', alldata, function (err, result) {
+            if (err) console.log('error', err);
+        });
     });
-});
