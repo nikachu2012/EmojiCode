c2c460338add6842f0665e634ad2a048909c759c
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-15 21:02:40 +0900

    [update] toolboxの更新

--- emojicode_stub.js
+++ emojicode_stub.js
@@ -1,7 +1,7 @@
 /* 
     EmojiCode Block Stub
     Created by nikachu2012(https://github.com/nikachu2012)
-    Create time: Mon Aug 15 2022 20:31:29 GMT+0900 (日本標準時)
+    Create time: Mon Aug 15 2022 21:02:20 GMT+0900 (日本標準時)
 */
 
 
