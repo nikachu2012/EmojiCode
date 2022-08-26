ee2b7aa7737f91f6c0fc366ca815a52c633a3aef
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-08-26 14:45:38 +0900

    [add] soundタブのコードを追加

--- 8-sound/sound_stub.js
+++ 8-sound/sound_stub.js
@@ -1,60 +1,39 @@
-Blockly.JavaScript['sound_play'] = function(block) {
-  var value_soundname = Blockly.JavaScript.valueToCode(block, 'soundName', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript[`sound_play`] = function(block) {
+  var value_soundname = Blockly.JavaScript.valueToCode(block, `soundName`, Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `emojiau.playSound(${value_soundname});\n`;
   return code;
 };
 
-Blockly.JavaScript['sound_pause'] = function(block) {
+Blockly.JavaScript[`sound_tone`] = function(block) {
+  var value_onkai = Blockly.JavaScript.valueToCode(block, `onkai`, Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_select = block.getFieldValue(`select`);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+
+  if(dropdown_select == '1'){
+    var code = `emojiau.playNote(${value_onkai}, '1m')`
+  }
+  else if(dropdown_select == '2'){
+    var code = `emojiau.playNote(${value_onkai}, '2n')`
+  }
+  else if(dropdown_select == '4'){
+    var code = `emojiau.playNote(${value_onkai}, '4n')`
+  }
+  else if(dropdown_select == '8'){
+    var code = `emojiau.playNote(${value_onkai}, '8n')`
+  }
+  else if(dropdown_select == '16'){
+    var code = `emojiau.playNote(${value_onkai}, '16n')`
+  }
+
+  var code = `...;\n`;
   return code;
 };
 
-Blockly.JavaScript['sound_effect_select'] = function(block) {
-  var value_soundname = Blockly.JavaScript.valueToCode(block, 'soundName', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_select = block.getFieldValue('select');
-  var value_effect_level = Blockly.JavaScript.valueToCode(block, 'effect_level', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript[`sound_tone_second`] = function(block) {
+  var value_onkai = Blockly.JavaScript.valueToCode(block, `onkai`, Blockly.JavaScript.ORDER_ATOMIC);
+  var value_second = Blockly.JavaScript.valueToCode(block, `second`, Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['sound_effect_dutu'] = function(block) {
-  var value_soundname = Blockly.JavaScript.valueToCode(block, 'soundName', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_select = block.getFieldValue('select');
-  var value_effect_level = Blockly.JavaScript.valueToCode(block, 'effect_level', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['sound_volume_select'] = function(block) {
-  var value_volume = Blockly.JavaScript.valueToCode(block, 'volume', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['sound_volume_dutu'] = function(block) {
-  var value_volume = Blockly.JavaScript.valueToCode(block, 'volume', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['sound_tone'] = function(block) {
-  var value_onkai = Blockly.JavaScript.valueToCode(block, 'onkai', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_select = block.getFieldValue('select');
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
-Blockly.JavaScript['sound_tone_second'] = function(block) {
-  var value_onkai = Blockly.JavaScript.valueToCode(block, 'onkai', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_second = Blockly.JavaScript.valueToCode(block, 'second', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `emojiau.playNoteSecond(${value_onkai}, ${value_second});\n`;
   return code;
 };
