ba31e9fa303ae063d2ea452c0feb4994d8374d2d
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-11-24 23:40:54 +0900

    [fix] `二重問題を解決

--- 7-sprite/sprite_stub.js
+++ 7-sprite/sprite_stub.js
@@ -162,7 +162,7 @@ Blockly.JavaScript['sprite_collision_boolean'] = function(block) {
   var value_sprite1 = Blockly.JavaScript.valueToCode(block, 'sprite1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_sprite2 = Blockly.JavaScript.valueToCode(block, 'sprite2', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = `emojisp.collision(\`${value_sprite1}\`,\`${value_sprite2}\`)\n`;
+  var code = `emojisp.collision(${value_sprite1}, ${value_sprite2})\n`;
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
