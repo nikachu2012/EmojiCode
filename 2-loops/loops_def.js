2b2add34fd853bd061de216e44bf2898add7b8b2
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2022-11-24 21:25:34 +0900

    [delete] repeat_settingを削除

--- 2-loops/loops_def.js
+++ 2-loops/loops_def.js
@@ -37,34 +37,6 @@ Blockly.Blocks['repeat_infinity'] = {
     }
   };
 
-Blockly.Blocks['repeat_setting'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldImage("https://img.icons8.com/material-outlined/48/000000/update-left-rotation.png", 16, 16, { alt: "*", flipRtl: "FALSE" }))
-        .appendField(new Blockly.FieldVariable("i"), "various")
-        .appendField("を");
-    this.appendValueInput("start")
-        .setCheck("Number");
-    this.appendDummyInput()
-        .appendField("から");
-    this.appendValueInput("end")
-        .setCheck("Number");
-    this.appendDummyInput()
-        .appendField("まで");
-    this.appendValueInput("dutu")
-        .setCheck("Number");
-    this.appendDummyInput()
-        .appendField("ずつ");
-    this.appendStatementInput("do")
-        .setCheck(null);
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(120);
- this.setTooltip("細かく指定してループを作成することができます。");
- this.setHelpUrl("https://example.com");
-  }
-};
   Blockly.Blocks['repeat_true'] = {
     init: function() {
       this.appendValueInput("NAME")
